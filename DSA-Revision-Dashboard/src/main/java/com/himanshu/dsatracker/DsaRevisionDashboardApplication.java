49cab2f014949101a67db67b59fa6e3ef3314485
newProject-demo <hima95shu@gmail.com>
2026-02-19 15:26:46 +0530

    Initial Spring Boot commit

---
+++ DSA-Revision-Dashboard/src/main/java/com/himanshu/dsatracker/DsaRevisionDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.himanshu.dsatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaRevisionDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaRevisionDashboardApplication.class, args);
+	}
+
+}
---
+++ DSA-Revision-Dashboard/src/test/java/com/himanshu/dsatracker/DsaRevisionDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.dsatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaRevisionDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
